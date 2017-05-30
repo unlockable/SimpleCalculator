ac57095a8805d204dd94d4c81ed68b62e997ab0c
unlockable <unlockable@naver.com>
2017-05-30 02:23:25 -0700

    Improve_my_fscanf

--- my_fscanf.c
+++ my_fscanf.c
@@ -6,7 +6,7 @@ void my_fscanf(FILE *fp, const char *format, ...) {
 	va_list list;
 	va_start(list, format);
 	while (*format) {
-		if (*format == '%') {
+		if (*format == '%') {		
 			int c;
 			format++;
 			switch (*format) {
@@ -20,24 +20,72 @@ void my_fscanf(FILE *fp, const char *format, ...) {
 					*addr = *addr * 10 + c - '0';
 					c = getc(fp);
 				}
-				
+
 				while (isspace(c = getc(fp))) {}
 
+				format++;
 				ungetc(c, fp);
 				break;
 			}
 			case 'c':
-				{
-					while (isspace(c = getc(fp))) {}
+			{
+				while (isspace(c = getc(fp))) {}
 
-					char* addr = va_arg(list, char*);
-					*addr = c;
-					break;
-				}
+				char* addr = va_arg(list, char*);
+				*addr = c;
+				format++;
+				break;
 			}
+			case 's':
+			{
+				char* addr = va_arg(list, char*);
+				int cnt = 1;
+				while (isspace(c = getc(fp))) {}
+
+				addr[0] = c;
+
+				while (!isspace(c = getc(fp))) {
+					addr[cnt] = c;
+					cnt++;
+				}
+				addr[cnt] = 0; //add null char
+				format++;
+				ungetc(c, fp);
+				break;
+			}
+			case 'f':
+			{
+				double* addr = va_arg(list, double*);
+				double decimalFraction = 0.1;
+
+				while (isspace(c = getc(fp))) {}
+				*addr = 0;
+				while (isdigit(c)) {
+					*addr = *addr * 10 + c - '0';
+					c = getc(fp);
+				}
+
+				if (c == '.') {
+					c = getc(fp);
+					while (isdigit(c)) {
+						*addr = *addr + (c - '0') * decimalFraction;
+						decimalFraction *= 0.1;
+						c = getc(fp);
+					}
+				}
+				format++;
+				ungetc(c, fp);
+				break;
+			}
+
+			}
+
 		}
-		else
-			format++;
+		else {
+			int c = getc(fp);
+			if (*format == c)
+				format++;
+		}
 	}
 	va_end(list);
 }
--- operators.h
+++
@@ -1,4 +0,0 @@
-double add(int op1, int op2);
-double mul(int op1, int op2);
-double div(int op1, int op2);
-double minus(int op1, int op2);
